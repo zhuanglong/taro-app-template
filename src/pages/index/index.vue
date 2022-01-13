448fad15a4c154410a532964b51c336a01d9d8b8
zl <zl@xx.com>
2022-01-13 14:35:01 +0800

    style: eslint fix

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,15 +5,15 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-import './index.scss'
+  import { ref } from 'vue';
+  import './index.scss';
 
-export default {
-  setup () {
-    const msg = ref('Hello world')
-    return {
-      msg
-    }
-  }
-}
+  export default {
+    setup() {
+      const msg = ref('Hello world');
+      return {
+        msg,
+      };
+    },
+  };
 </script>
